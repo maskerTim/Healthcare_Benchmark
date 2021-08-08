054f738c198b4d7824a58f74f5fd2db3ec42935d
maskerTim <masker2003@gmail.com>
2021-08-09 00:27:07 +0800

    update: video actuator

--- actuators/actuatorFactory.py
+++ actuators/actuatorFactory.py
@@ -1,10 +1,12 @@
 from .models.heartActuator import HeartActuator
 from .models.bloodPressureActuator import BloodPressureActuator
+from .models.videoActuator import VideoActuator
 
 def ActuatorFactory(actuator):
     """ Factory for making sensors """
     actuators = {
         "HA": HeartActuator(),
-        "BA": BloodPressureActuator()
+        "BA": BloodPressureActuator(),
+        "VA": VideoActuator()
     }
     return actuators[actuator]
